abff594cb4c6172b0210e5a3b4ba01ec784bc38e
JeannotDu67 <44265577+JeannotDu67@users.noreply.github.com>
2020-04-08 18:46:17 +0200

    push

--- puissance/trigger.m
+++ puissance/trigger.m
@@ -7,10 +7,10 @@ R1B = 12000;
 R1 = R1A*R1B/(R1A+R1B);
 Vin_max = R1B/(R1A+R1B)*Vm
 
-Vtl = 0;
+Vtl = 0.2;
 Vth = 2.7;
 R3 = R1*Vdd/(Vth-Vtl)
 
-Vref = Vth*R3/(R1+R3);
+Vref = Vth*R3/(R1+R3)
 R2 = 10000*(Vdd/Vref-1)
 
